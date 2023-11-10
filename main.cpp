7afb7a4c82c65118f15e2349a521b3ed4cf5beab
jvyden <jvyden@jvyden.xyz>
2023-11-09 22:39:42 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // I told you. It's literally a hello world right now.
+    // There's nothing here in this project yet. Go away.
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
